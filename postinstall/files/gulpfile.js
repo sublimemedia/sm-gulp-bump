2f7cad9ae98badf0d6a3381bb03597c776d059dd
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-02 18:08:42 -0700

    working on script

--- postinstall/files/gulpfile.js
+++ postinstall/files/gulpfile.js
@@ -1,5 +1,13 @@
 'use strict'
+const fs = require('fs')
 const gulp = require('gulp')
-const currentVer = require('./package.json').version
+const pkg = require('./package.json')
+const repoOrigin = require('remote-origin-url').sync()
+if (repoOrigin) {
+  pkg.name = repoOrigin.match(/.+\/(.+)\.git/)[1]
+  pkg.repository.url = 'git+' + repoOrigin
+  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
+}
+const currentVer = pkg.version
 const bumpTask = require('sm-gulp-bump')({ currentVer })
 gulp.task('push', bumpTask)
