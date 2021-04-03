947de46a152679b62ae8d18d0279b22894749c5e
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-02 17:48:49 -0700

    fixed bug

--- postinstall/index.js
+++ postinstall/index.js
@@ -6,11 +6,10 @@ const filesToCopy = ['./files/gulpfile.js', './files/Ω-git-bump-push.bat']
 
 // User's local directory
 const userPath = process.env.INIT_CWD
-
-const pkg = require(path.join(userPath, '/package.json'))
-pkg.name = repoOrigin.match(/.+\/(.+)\.git/)[1]
-  pkg.repository.url = 'git+' + repoOrigin
-  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
+const pkgPath = path.join(userPath, '/package.json')
+const pkg = require(pkgPath)
+pkg.scripts.push = 'gulp push'
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
 
 // Moving files to user's local directory
-gentlyCopy(filesToCopy, userPath, {overwrite: true})
+gentlyCopy(filesToCopy, userPath, { overwrite: true })
