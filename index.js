437647894b6ab81cf9d1c39af618781d134e8a8d
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-07 16:00:20 -0700

    updated some linting stuff

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-'use strict';
+'use strict'
 
 // var Promise = require('bluebird');
-var gulp = require('gulp');
-var versionBump = require('./lib/bump');
-var versionTag = require('./lib/tag');
-var gitCommit = require('./lib/git-commit');
-var gitPush = require('./lib/git-push');
+const gulp = require('gulp')
+const versionBump = require('./lib/bump')
+const versionTag = require('./lib/tag')
+const gitCommit = require('./lib/git-commit')
+const gitPush = require('./lib/git-push')
 
 exports = module.exports = function (opts) {
   if (!opts || typeof opts !== 'object') opts = {}
-  if (typeof opts.currentVer === 'undefined') opts.currentVer = 'Not supplied';
-  if (typeof opts.logger === 'undefined') opts.logger = console;
-  var verBumped = false;
-  var push = function (done) {
+  if (typeof opts.currentVer === 'undefined') opts.currentVer = 'Not supplied'
+  if (typeof opts.logger === 'undefined') opts.logger = console
+  let verBumped = false
+  const push = function (done) {
     return versionBump(gulp, opts.currentVer, opts.logger)
-      .then(function (bumped){
+      .then(function (bumped) {
         verBumped = bumped
         return gitCommit(opts.logger, verBumped, opts.currentVer)
       })
       .then(function () {
         if (verBumped) {
-          return versionTag(gulp, opts.logger);
+          return versionTag(gulp, opts.logger)
         } else {
-          return Promise.resolve(false);
+          return Promise.resolve(false)
         }
       })
       .then(function () {
-        return gitPush(opts.logger);
+        return gitPush(opts.logger)
       })
       .then(function () {
-        done();
-        return null;
+        done()
+        return null
       })
       .catch(function (err) {
-        opts.logger.error(err);
-        done();
-      });
-  };
-  return push;
-};
+        opts.logger.error(err)
+        done()
+      })
+  }
+  return push
+}
